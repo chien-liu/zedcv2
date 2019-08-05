6007b22f287ddc7f68cc9be71ab07fe884be0071
liu_chien <liu_chien@gapp.nthu.edu.tw>
2019-08-05 16:50:37 +0800

    First commit

---
+++ zedcv2.py
@@ -0,0 +1,83 @@
+import numpy as np
+import cv2
+
+class Camera():
+    def __init__(self,):
+        self.cap = cv2.VideoCapture(1)
+        assert self.cap.isOpened(), "Can't reach ZED camera."
+
+        # Default resolution 2K
+        self.set_resolution("2K")
+        # Default using left eye
+        self.set_eye("left")
+
+    def __del__(self,):
+        self.cap.release()
+
+    def set_resolution(self, resolution):
+        if resolution == "2K":
+            width = 4416
+            height = 1242
+        elif resolution == "1080p":
+            width = 3840
+            height = 1080
+        elif resolution == "720p":
+            width = 2560
+            height = 720
+        elif resolution == "WVGA":
+            width = 1344
+            height = 376
+        else:
+            print("Unsupported resolution type")
+            raise NameError
+
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        
+        self._width = width
+        self._height = height
+
+
+    def set_eye(self, eye):
+        assert eye in ["left", "right", "both"], "Unsupported type"
+        self._eye = eye
+       
+
+    def open(self,):
+        pass
+
+    def retrieve_image(self,):
+        ret, frame = self.cap.read()   
+        
+        if ret:
+            if self._eye == "left":
+                frame = frame[:, :int(self._width/2), :]
+            elif self._eye == "right":
+                frame = frame[:, int(self._width/2):, :]
+            elif self._eye == "both":
+                frame = frame
+            else:
+                raise NotImplementedError
+
+            return frame
+        else:
+            print("Can't reach image.")
+            return 0
+
+    
+
+        
+if __name__ == "__main__":
+    zed = Camera()
+    zed.set_resolution("WVGA")
+    zed.set_eye("left")
+
+
+    while(True):
+        # Capture frame-by-frame
+        frame = zed.retrieve_image()
+
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
---
+++ zednode.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import sys
+import cv2
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+from zedcv2 import Camera
+
+class image_converter:
+
+    def __init__(self):
+        # ROS
+        self.image_pub = rospy.Publisher("image_topic_2",Image)
+        self.bridge = CvBridge()
+        # zedcv2
+        self.zed = Camera()
+        self.zed.set_resolution("2K")    # "2K", "1080p", "720p", "WVGA"
+        self.zed.set_eye("left")         # "left", "right", "both"
+
+    def get_image(self):
+        # Capture frame-by-frame
+        cv2_image = self.zed.retrieve_image()
+        self.callback(cv2_image)
+
+    def callback(self, cv2_image):
+        try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv2_image, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
+
+def main(args):
+    ic = image_converter()
+    
+    rospy.init_node('zedcv2_raw_image', anonymous=True)
+
+    r = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        ic.get_image()
+        r.sleep()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
